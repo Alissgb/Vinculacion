4e611ef93df9cf5e13620e20e678514c8b12e8b7
23456-Tony-Larrea <avs.larrea@yavirac.edu.ec>
2022-09-07 01:56:30 -0500

    only image in register form

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -12,14 +12,14 @@
     </button>
     <div class="collapse navbar-collapse justify-content-end">
       <ul class="navbar-nav">
-        <li class="nav-item">
+        {{-- <li class="nav-item">
           <a href="{{ route('home') }}" class="nav-link">
             <i class="material-icons">dashboard</i> {{ __('Dashboard') }}
           </a>
         </li>
-        <li class="nav-item{{ $activePage == 'register' ? ' active' : '' }}">
+         --}}<li class="nav-item{{ $activePage == 'register' ? ' active' : '' }}">
           <a href="{{ route('register') }}" class="nav-link">
-            <i class="material-icons">person_add</i> {{ __('Register') }}
+            <i class="material-icons">person_add</i> {{ __('Registro') }}
           </a>
         </li>
         <li class="nav-item{{ $activePage == 'login' ? ' active' : '' }}">
@@ -27,12 +27,12 @@
             <i class="material-icons">fingerprint</i> {{ __('Login') }}
           </a>
         </li>
-        <li class="nav-item ">
+        {{-- <li class="nav-item ">
           <a href="#" class="nav-link">
             <i class="material-icons">face</i> {{ __('Profile') }}
           </a>
         </li>
-      </ul>
+       --}}</ul>
     </div>
   </div>
 </nav>
